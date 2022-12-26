f558097fc581312d238b1527b146eedf0e1732e1
nitrophenol <mayanksingh0713@gmail.com>
2022-12-26 16:02:15 +0530

    hh

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const hbs=require("hbs")
+const path=require("path")
+const mongoose=require("mongoose")
+const app = express();
+const uri = require('./config/key').mongoURI;
+mongoose.connect(uri, {useNewUrlParser:true})
+.then(()=>console.log('hey'))
+.catch(err=>console.log(err))
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+
+  });
+  const User = mongoose.model('User', UserSchema);
+
+const port = 80;
+ app.use('/static',express.static('static'))
+ app.set('view engine','hbs')
+ app.use(express.urlencoded({ extended:false}))
+ mongoose.set('strictQuery', false);
+ app.set("views",path.join(__dirname,"views"))
+app.get("/", (req, res)=>{ 
+    res.status(200).render("index");
+});
+app.post("/", (req, res)=>{
+    nam=req.body.name
+    email=req.body.email
+    password=req.body.password
+    console.log(email,password);
+    const us= new User({
+        name:`${nam}`,
+        email:`${email}`,
+        password:`${password}`
+    })
+    us.save()
+    res.render("index")
+});
+
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
---
+++ config/key.js
@@ -0,0 +1,4 @@
+const db="mongodb+srv://nitro:saymyname@nodeproject.7q1tbxc.mongodb.net/?retryWrites=true&w=majority"
+module.exports = {
+    mongoURI: db
+};
